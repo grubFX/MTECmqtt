41b2ba5996e9d7a83828ecd6f2667eec871581ca
Felix Gruber <2887162+grubFX@users.noreply.github.com>
2024-01-16 22:41:46 +0000

    Update config_flow with default values

--- custom_components/MTEC/config_flow.py
+++ custom_components/MTEC/config_flow.py
@@ -18,14 +18,19 @@ class MTECConfigFlow(config_entries.ConfigFlow, domain="MTEC"):
             step_id="user_modbus",
             data_schema=vol.Schema(
                 {
-                    vol.Required("ip"): str,
-                    vol.Required("port"): int,
-                    vol.Required("slave"): int,
-                    vol.Required("timeout"): int,
-                    vol.Required("retries"): int,
-                    vol.Required("framer"): int,
+                    vol.Required("ip", default="espressif.local"): str,
+                    vol.Required("port", default=5743): vol.All(
+                        vol.Coerce(int), vol.Range(min=1, max=65535)
+                    ),
+                    vol.Required("slave", default=252): int,
+                    vol.Required("timeout", default=5): int,
+                    vol.Required("retries", default=3): int,
+                    vol.Required("framer", default="rtu"): vol.In(
+                        ["ascii", "binary", "rtu", "socket", "tls"]
+                    ),
                 }
             ),
+            description_placeholders=user_input,
         )
 
     async def async_step_user_refresh(self, user_input=None):
@@ -38,10 +43,12 @@ class MTECConfigFlow(config_entries.ConfigFlow, domain="MTEC"):
             step_id="user_refresh",
             data_schema=vol.Schema(
                 {
-                    vol.Required("now_s"): int,
-                    vol.Required("day_m"): int,
-                    vol.Required("total_m"): int,
-                    vol.Required("config_m"): int,
+                    vol.Required("now_s", default=10): vol.All(
+                        vol.Coerce(int), vol.Range(min=1)
+                    ),
+                    vol.Required("day_m", default=5): int,
+                    vol.Required("total_m", default=5): int,
+                    vol.Required("config_m", default=60): int,
                 }
             ),
         )
@@ -56,11 +63,11 @@ class MTECConfigFlow(config_entries.ConfigFlow, domain="MTEC"):
             step_id="user_extended_data",
             data_schema=vol.Schema(
                 {
-                    vol.Required("grid_data"): bool,
-                    vol.Required("inverter_data"): bool,
-                    vol.Required("backup_data"): bool,
-                    vol.Required("battery_data"): bool,
-                    vol.Required("pv_data"): bool,
+                    vol.Required("grid_data", default=True): bool,
+                    vol.Required("inverter_data", default=True): bool,
+                    vol.Required("backup_data", default=True): bool,
+                    vol.Required("battery_data", default=True): bool,
+                    vol.Required("pv_data", default=True): bool,
                 }
             ),
         )
